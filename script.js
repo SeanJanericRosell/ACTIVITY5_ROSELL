4c6c8409da8d57a088835a08adfe01bab881b183
Sean Janeric Rosell <act.sjrosell@gmail.com>
2024-10-01 18:37:48 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
 $(document).ready(function () {
-    $.getJSON('pokedex.json', function (data) {
-        const pokemonContainer = $('#pokemon-container');
 
+    $.getJSON('pokedex.json', function (data) {
+        const pokemonList = $('#pokemon-list');
         data.forEach(pokemon => {
-            const testCard = `
-                <div class="pokemon-card">
-                    <img src="${pokemon.image}" alt="${pokemon.name}">
-                    <ul>
-                        <li class="p-name"><a href="pokemon.html?id=${pokemon.id}">${pokemon.name}</a></li>
-                        <li class="p-id">${pokemon.id}</li>
-                        <li class="p-type">${pokemon.type.map(type => `<img src="images/${type.toLowerCase()}.png" alt="${type}">`).join('')}</li>
-                    </ul>
-                </div>
-            `;
-            pokemonContainer.append(testCard);
+            const card = `<div class="pokemon-card">
+                            <a href="pokemon.html?id=${pokemon.id}">${pokemon.name}</a>
+                          </div>`;
+            pokemonList.append(card);
         });
     });
+
+    
+    const urlParams = new URLSearchParams(window.location.search);
+    const pokemonId = urlParams.get('id');
+
+    if (pokemonId) {
+        $.getJSON('pokedex.json', function (data) {
+            const selectedPokemon = data.find(pokemon => pokemon.id == pokemonId);
+            if (selectedPokemon) {
+                const details = `<h2>${selectedPokemon.name}</h2>
+                                <img src="${selectedPokemon.image}" alt="${selectedPokemon.name}">
+                                <p>Type: ${selectedPokemon.type.join(', ')}</p>
+                                <p>Description: ${selectedPokemon.description}</p>
+                                <p>Stats:</p>
+                                <ul>
+                                    <li>HP: ${selectedPokemon.stats.hp}</li>
+                                    <li>Attack: ${selectedPokemon.stats.attack}</li>
+                                    <li>Defense: ${selectedPokemon.stats.defense}</li>
+                                    <li>Speed: ${selectedPokemon.stats.speed}</li>
+                                </ul>`;
+                $('#pokemon-details').html(details);
+            }
+        });
+    }
 });
